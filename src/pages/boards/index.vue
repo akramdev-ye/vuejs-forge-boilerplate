9b11da1da8389d99b45742a2eee4a8d23242d4e3
Akram Ghaleb <46090581+akramdev-ye@users.noreply.github.com>
2022-07-13 22:24:50 +0300

    fixing issues

--- src/pages/boards/index.vue
+++ src/pages/boards/index.vue
@@ -42,22 +42,34 @@ const boards = ref<Partial<Board>[]>([
   {
     id: "3",
     title: "My Third Board",
-    order: "https://picsum.photos/480/270?watermelon=3",
+    order: "[]",
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?watermelon=3",
+    },
   },
   {
     id: "4",
     title: "And another one",
-    order: "https://picsum.photos/480/270?watermelon=4",
+    order: "[]",
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?watermelon=4",
+    },
   },
   {
     id: "5",
     title: "Cute boardie",
-    order: "https://picsum.photos/480/270?watermelon=5",
+    order: "[]",
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?watermelon=5",
+    },
   },
   {
     id: "6",
     title: "Serious corpo board",
-    order: "https://picsum.photos/480/270?watermelon=6",
+    order: "[]",
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?watermelon=6",
+    },
   }
 ]);
 
